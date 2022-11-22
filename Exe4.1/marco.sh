aa34ab22873bb1ad8bde6fce215299f406e757b5
Teemu Koivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-11-22 17:25:08 +0200

    Exe4.1 Marco & Polo shell scripting (#6)

--- Exe4.1/marco.sh
+++ Exe4.1/marco.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-MARCO=$(pwd)
-echo "Current path: $MARCO"
-export MARCO="$MARCO"
+mkdir $HOME/bin
+export PATH=$PATH:$HOME/bin
+cp marco.sh $HOME/bin
+chmod +x $HOME/bin/marco.sh
+
+function marco() {
+    MARCO=$(pwd)
+    echo "Current path: $MARCO"
+    export MARCO="$MARCO"
+}
+
+function polo() {
+    MARCO=$(printenv MARCO)
+
+    if [ -z "$MARCO" ]
+    then
+        echo "Path is empty"
+    else
+        echo "Moving to: $MARCO"
+        cd $MARCO
+    fi
+}
--- Exe4.1/polo.sh
+++ Exe4.1/polo.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-MARCO=$(printenv MARCO)
-if [ -z "$MARCO" ]
-then
-      echo "Path is empty"
-else
-      echo "Moving to: $MARCO"
-      cd $MARCO
-fi
+mkdir $HOME/bin
+export PATH=$PATH:$HOME/bin
+cp polo.sh $HOME/bin
+chmod +x $HOME/bin/polo.sh
+
+function polo() {
+    MARCO=$(printenv MARCO)
+
+    if [ -z "$MARCO" ]
+    then
+        echo "Path is empty"
+    else
+        echo "Moving to: $MARCO"
+        cd $MARCO
+    fi
+}
